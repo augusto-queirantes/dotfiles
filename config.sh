c38942a336200a1b8faebc60c5230176abea32cc
Augusto Queirantes <augusto_queirantes@outlook.com>
2021-01-30 04:23:57 -0300

    Add shell script to configure dependencies

---
+++ config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+RESTORE='\033[0m'
+
+function echo_yellow {
+  YELLOW='\e[0;33m'
+
+  echo -e "${YELLOW}$1${RESTORE}";
+}
+
+function echo_green {
+  GREEN='\033[00;32m'
+
+  echo -e "${GREEN}$1${RESTORE}";
+}
+
+echo_yellow 'Starting Neovim configuration'
+
+sh $(pwd)/nvim/config.sh
+
+echo_green 'Neovim were successfully configured'
---
+++ nvim/config.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ln -sf $(pwd)/nvim/ ~/.config/
