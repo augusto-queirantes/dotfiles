690abdbf0ed9a3cb3741ca95d1c18c4f4062e6ba
Augusto Queirantes <augusto.queirantes@gmail.com>
2021-02-04 19:39:33 -0300

    Update file links

--- terminal/config.sh
+++ terminal/config.sh
@@ -8,8 +8,8 @@ git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions $(pwd)/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $(pwd)/themes/powerlevel10k
 
-ln -sf $(pwd)/plugins/ ~/.oh-my-zsh/custom/plugins
-ln -sf $(pwd)/themes/ ~/.oh-my-zsh/custom/themes
+ln -sf $(pwd)/plugins/* ~/.oh-my-zsh/custom/plugins
+ln -sf $(pwd)/themes/* ~/.oh-my-zsh/custom/themes
 ln -sf $(pwd)/.zshrc ~/.zshrc
 
 chsh -s $(which zsh)
