a45431225970f608686df4fa753bc7c91dc5fb87
Augusto Queirantes <augusto.queirantes@gmail.com>
2024-05-18 18:58:58 -0300

    Add neovim installation dependencies

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
 # Dependencies installation
 
-sudo apt install -y curl zsh git ripgrep
+sudo apt upgrade -y && sudo apt update -y
+
+# System dependencies
+
+sudo apt install -y curl zsh git ripgrep software-properties-common python3-dev python3-pip
 
 # Oh my zsh installation
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Neovim installation
+
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt update -y
+sudo apt install neovim
