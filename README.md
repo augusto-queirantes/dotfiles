66d27b328ad45af7beb0902abe23bc92a14acd3a
Augusto Queirantes <augusto.queirantes@gmail.com>
2024-05-18 18:27:08 -0300

    Add oh my zsh basic installation

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dotfiles
+
+That's my dotfiles. Use the following instructions to properly install it:
+
+1. Execute the installation script
+
+`./install.sh`
---
+++ install.sh
@@ -0,0 +1,7 @@
+# Dependencies installation
+
+sudo apt install -y curl zsh git
+
+# Oh my zsh installation
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
