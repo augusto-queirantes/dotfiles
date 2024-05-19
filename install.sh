17a4d6b00edd5757fa072b01b70e73da3368440c
Augusto Queirantes <augusto.queirantes@gmail.com>
2024-05-19 09:54:15 -0300

    Move installation scripts to a unique file

--- install.sh
+++ install.sh
@@ -2,16 +2,24 @@
 
 sudo apt upgrade -y && sudo apt update -y
 
-# System dependencies
+## System dependencies
 
 sudo apt install -y curl zsh git ripgrep software-properties-common python3-dev python3-pip
 
-# Oh my zsh installation
+## Oh my zsh installation
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Neovim installation
+## Neovim installation
 
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt update -y
 sudo apt install neovim
+
+# Configuration
+
+## Oh my zsh configuration
+
+ln -s ./zsh/zshrc ~/.zshr
+
+## Nvim configuration
