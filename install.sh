863645a4d4c3e69d7cdc17bc88b089106d7b63aa
Augusto Queirantes <augusto.queirantes@gmail.com>
2024-05-19 20:20:10 -0300

    Add font configuration script

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+
 # Dependencies installation
 
 sudo apt upgrade -y && sudo apt update -y
 
 ## System dependencies
 
-sudo apt install -y curl zsh git ripgrep software-properties-common python3-dev python3-pip jq fonts-firacode
+sudo apt install -y curl zsh git ripgrep software-properties-common python3-dev python3-pip jq
 
 ## Neovim installation
 
@@ -23,3 +25,22 @@ ln -fs $PWD/zsh/zshrc ~/.zshrc
 ## Nvim configuration
 
 ln -fs $PWD/nvim ~/.config/nvim
+
+## Font configuration
+
+version=$(curl -s "https://api.github.com/repos/ryanoasis/nerd-fonts/tags" | jq -r '.[0].name')
+fonts_dir="${HOME}/.local/share/fonts"
+file_name="FiraMono.zip"
+download_url="https://github.com/ryanoasis/nerd-fonts/releases/download/${version}/${file_name}"
+
+if [[ ! -d "$fonts_dir" ]]; then
+  mkdir -p "$fonts_dir"
+fi
+
+wget --quiet "$download_url" -O "$file_name"
+
+unzip "$file_name" -d "$fonts_dir"
+
+rm "$file_name"
+
+fc-cache -fv
