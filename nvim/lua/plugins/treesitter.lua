472591f8eef0f6db47f3b84aa326e2fe3e59ee1b
Augusto Queirantes <augusto.queirantes@gmail.com>
2024-06-19 20:10:46 -0300

    Improve treesiter configuration

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,31 +1,32 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    version = false,
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        indent = {
-          enable = true,
-        },
-        sync_install = false,
-        auto_install = true,
-        ensure_installed = {
-          "html",
-          "javascript",
-          "json",
-          "lua",
-          "tsx",
-          "typescript",
-          "vim",
-        },
-      })
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		version = false,
+		build = ":TSUpdate",
+		event = { "BufReadPost", "BufNewFile" },
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			---@diagnostic disable-next-line: missing-fields
+			config.setup({
+				highlight = {
+					enable = true,
+				},
+				indent = {
+					enable = false,
+				},
+				sync_install = false,
+				auto_install = true,
+				ensure_installed = {
+					"html",
+					"javascript",
+					"json",
+					"lua",
+					"tsx",
+					"typescript",
+					"vim",
+					"elixir",
+				},
+			})
+		end,
+	},
 }
