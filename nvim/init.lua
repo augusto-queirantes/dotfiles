4ff1a82ae6a3afab9d4559e2915eff0bcad79190
Augusto Queirantes <augusto.queirantes@gmail.com>
2024-06-19 20:11:02 -0300

    Add missing import

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,5 @@
 require("config.general")
 require("config.keybindings")
 require("config.lazy")
+require("config.text")
+require("config.auto-commands")
--- nvim/lua/config/text.lua
+++ nvim/lua/config/text.lua
@@ -5,15 +5,11 @@ vim_options.tabstop = 2
 vim_options.softtabstop = 2
 vim_options.shiftwidth = 2
 vim_options.expandtab = true
-vim_options.smarttab = true
+vim_options.smartindent = true
 
 -- Sets line break on 1000 characters
 vim_options.lbr = true
 vim_options.tw = 1000
-
--- Indentation
-vim_options.autoindent = true
-vim_options.smartindent = true
 
 -- Encoding
 vim_options.encoding = "utf-8"
