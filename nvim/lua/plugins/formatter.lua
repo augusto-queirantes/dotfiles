50fda1407836805cd6d93dedcf919c3880c5f31a
Augusto Queirantes <augusto.queirantes@gmail.com>
2024-06-19 20:09:02 -0300

    Add formatter

---
+++ nvim/lua/plugins/formatter.lua
@@ -0,0 +1,16 @@
+return {
+	"stevearc/conform.nvim",
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				["*"] = { "codespell" },
+				["_"] = { "trim_whitespace" },
+			},
+			format_on_save = {
+				lsp_format = "fallback",
+				timeout_ms = 500,
+			},
+		})
+	end,
+}
