acd3dc6da52fa60c5e8e1652340d9cc2cc8bda7a
Augusto Queirantes <augusto.queirantes@gmail.com>
2024-06-25 21:02:27 -0300

    Properly setup elixirls

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,29 +1,35 @@
 return {
-	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"folke/lazydev.nvim",
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	config = function()
-		local servers = { "typos_lsp", "elixirls", "lua_ls" }
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "folke/lazydev.nvim",
+    "hrsh7th/cmp-nvim-lsp",
+  },
+  config = function()
+    local servers = { "lua_ls" }
 
-		require("mason").setup()
+    require("mason").setup()
 
-		require("mason-lspconfig").setup({
-			ensure_installed = servers,
-		})
+    require("mason-lspconfig").setup({
+      ensure_installed = servers,
+    })
 
-		require("lazydev").setup()
+    require("lazydev").setup()
 
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-		for _, server in pairs(servers) do
-			require("lspconfig")[server].setup({
-				capabilities = capabilities,
-			})
-		end
-	end,
+    for _, server in pairs(servers) do
+      require("lspconfig")[server].setup({
+        capabilities = capabilities,
+      })
+    end
+
+    -- Elixirls must be handled different because the lsp server is installed manually
+    require("lspconfig")["elixirls"].setup({
+      capabilities = capabilities,
+      cmd = { "/home/augusto/.elixirls/language_server.sh" },
+    })
+  end,
 }
--- nvim/scripts/elixir_lsp.sh
+++ nvim/scripts/elixir_lsp.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-# Install elixir lang server
+# Clones the repo
 git clone git@github.com:elixir-lsp/elixir-ls.git ~/.elixir-ls
+
+# Creates the builded files dir
+mkdir ~/.elixirls
 
 cd ~/.elixir-ls
 
-# Install dependencies and compile it
+# Install dependencies
 mix deps.get
-mix compile && mix elixir_ls.release -o release
+
+# Compile it
+MIX_ENV=prod mix compile
+
+# Release it inside the specified folder
+MIX_ENV=prod mix elixir_ls.release2 -o ~/.elixirls
 
 # Go back
 cd ~/dotfiles
+
+rm -rf ~/.elixir-ls/
