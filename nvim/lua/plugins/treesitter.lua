b3bc407019edd617ed9d965eacf996c1a7862224
Augusto Queirantes <augusto.queirantes@gmail.com>
2024-06-30 10:41:15 -0300

    Improve tessiter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,32 +1,32 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		version = false,
-		build = ":TSUpdate",
-		event = { "BufReadPost", "BufNewFile" },
-		config = function()
-			local config = require("nvim-treesitter.configs")
-			---@diagnostic disable-next-line: missing-fields
-			config.setup({
-				highlight = {
-					enable = true,
-				},
-				indent = {
-					enable = false,
-				},
-				sync_install = false,
-				auto_install = true,
-				ensure_installed = {
-					"html",
-					"javascript",
-					"json",
-					"lua",
-					"tsx",
-					"typescript",
-					"vim",
-					"elixir",
-				},
-			})
-		end,
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    version = false,
+    build = ":TSUpdate",
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      ---@diagnostic disable-next-line: missing-fields
+      config.setup({
+        highlight = {
+          enable = true,
+        },
+        indent = {
+          enable = false,
+        },
+        sync_install = false,
+        auto_install = true,
+        ensure_installed = {
+          "html",
+          "javascript",
+          "json",
+          "lua",
+          "tsx",
+          "typescript",
+          "vim",
+          "elixir",
+          "dockerfile"
+        },
+      })
+    end,
+  },
 }
