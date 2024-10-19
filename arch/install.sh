c6b79de4501d22c2dd634a7691ab3d480f7cd404
Augusto Queirantes <augusto.queirantes@gmail.com>
2024-10-19 17:45:51 -0300

    Update installation script

--- arch/install.sh
+++ arch/install.sh
@@ -2,7 +2,7 @@
 
 # Dependencies related
 
-sudo pacman -S curl zsh git ripgrep jq meld zsh-syntax-highlighting
+sudo pacman -S curl zsh git ripgrep jq meld zsh-syntax-highlighting unzip make base-devel libssh unixodbc libxslt fop docker-buildx docker-compose docker
 
 # Oh my zsh
 
@@ -17,25 +17,16 @@ ln -fs $PWD/zsh/zshrc ~/.zshrc
 # Asdf configuration
 
 ## Clone asdf using the latest tag version as target branch
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch (git ls-remote --tags --sort="v:refname" https://github.com/asdf-vm/asdf.git | tail -n1 | sed 's/.*\///; s/\^{}//')
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 
 asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
 asdf plugin add elixir https://github.com/asdf-vm/asdf-elixir.git
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 
-asdf install erlang 27.0
-asdf install elixir 1.17.1
-asdf install nodejs 20.15.0
+asdf install erlang 24.3.4.16
+asdf install elixir 1.16.1-otp-24
+asdf install nodejs 18.20.3
 
-asdf global erlang 27.0
-asdf global elixir 1.17.1
-asdf global nodejs 20.15.0
-
-# Sublime
-sudo snap install sublime-text --classic
-
-# Discord
-sudo snap install discord
-
-# Spotify
-sudo snap install spotify
+asdf global erlang 24.3.4.16
+asdf global elixir 1.16.1-otp-24
+asdf global nodejs 18.20.3
